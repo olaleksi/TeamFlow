c0316cb4ee4db817105f6f40e1b143a0fff794a0
Olalekan <olaleksi4231.com>
2026-03-19 23:13:16 +0100

    made more deployment chnages

--- src/config/database.js
+++ src/config/database.js
@@ -3,8 +3,13 @@ import { PrismaClient } from "@prisma/client";
 
 // Initialize Prisma Client
 const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL, // This explicitly tells Prisma to use your Render database URL
+    },
+  },
   log:
-    process.env.NODE_ENV === "development"
+    process.env.NODE_ENV === "production"
       ? ["query", "error", "warn"]
       : ["error"],
 });
